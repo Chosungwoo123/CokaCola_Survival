4d838ef42f59f645c10a8174f888404f192b8874
chosungwoo <ejyoo12345@naver.com>
2023-07-04 18:23:06 +0900

    타이틀에서 게임씬 넘어가는거 구현 완료

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using EasyTransition;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TransitionSettings transition;
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameScene()
     {
-        
+        TransitionManager.Instance().Transition("PistolStage1", transition, 0f);
     }
 }
