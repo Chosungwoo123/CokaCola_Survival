5e63b4581c1e5ba36d5f9c455cbdc3ea98754db3
chosungwoo <ejyoo12345@naver.com>
2023-07-11 11:51:24 +0900

    타이틀 화면 애니메이션, EXP 슬라이더 로직 수정

--- Assets/Scripts/UI/ExpSlider.cs
+++ Assets/Scripts/UI/ExpSlider.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,6 +18,7 @@ public class ExpSlider : MonoBehaviour
         float curExp = GameManager.Instance.curExp;
         float maxExp = GameManager.Instance.nextExp[Mathf.Min(GameManager.Instance.playerLevel, GameManager.Instance.nextExp.Length)];
 
-        expBar.value = Mathf.Max(curExp / maxExp, 0.03f);
+        if (expBar.value > curExp / maxExp) expBar.value = 0.03f;
+        expBar.value = Mathf.Lerp(expBar.value, Mathf.Max(curExp / maxExp, 0.03f), Time.deltaTime * 12f );
     }
 }
---
+++ Assets/Scripts/UI/TitleManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform titleRect;
+
+    [SerializeField] private RectTransform[] buttonRects;
+
+    private void Start()
+    {
+        StartCoroutine(StartRoutine());
+    }
+
+    IEnumerator StartRoutine()
+    {
+        titleRect.localScale = new Vector3(0, 0, 0);
+
+        foreach (var item in buttonRects)
+        {
+            item.anchoredPosition = new Vector3(600, item.anchoredPosition.y, 0);
+        }
+
+        titleRect.DOScale(6f, 1.5f).SetDelay(1f).SetEase(Ease.OutElastic);
+
+        yield return new WaitForSeconds(2f);
+
+        int index = 0;
+        
+        for (int i = buttonRects.Length - 1; i >= 0; i--)
+        {
+            buttonRects[i].DOAnchorPosX(-84, 0.7f).SetDelay(index * 0.1f).SetEase(Ease.OutBack);
+            index++;
+        }
+
+        yield return null;
+    }
+}
