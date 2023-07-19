6d2e21f8e123e2e83c5d83da3f20a800b9be2f32
chosungwoo <ejyoo12345@naver.com>
2023-07-19 10:44:23 +0900

    스테이지 2 타워와 겹치면 투명해지는거 구현

---
+++ Assets/Scripts/Player/TowerCol.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCol : MonoBehaviour
+{
+    public SpriteRenderer towerSR;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {       
+        if (other.transform.CompareTag("Player"))
+        {
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float a = 1f;
+
+        while (a > 0.5f)
+        {
+            towerSR.color = new Vector4(towerSR.color.r, towerSR.color.g, towerSR.color.b, a);
+            a -= Time.deltaTime * 3;
+            yield return null;
+        }
+        
+        towerSR.color = new Vector4(towerSR.color.r, towerSR.color.g, towerSR.color.b, 0.5f);
+    }
+    
+    private IEnumerator FadeIn()
+    {
+        float a = 0.5f;
+
+        while (a < 1f)
+        {
+            towerSR.color = new Vector4(towerSR.color.r, towerSR.color.g, towerSR.color.b, a);
+            a += Time.deltaTime * 3;
+            yield return null;
+        }
+        
+        towerSR.color = new Vector4(towerSR.color.r, towerSR.color.g, towerSR.color.b, 1);
+    }
+}
